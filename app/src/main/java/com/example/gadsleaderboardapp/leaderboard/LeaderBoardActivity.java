14b28cfa9e2d7c45eeb99bdf35b6095a5a28f993
Sommie09 <ceekay2a7@gmail.com>
2020-08-30 21:08:55 +0100

    Redesigned submit screen

--- app/src/main/java/com/example/gadsleaderboardapp/leaderboard/LeaderBoardActivity.java
+++ app/src/main/java/com/example/gadsleaderboardapp/leaderboard/LeaderBoardActivity.java
@@ -4,25 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.gadsleaderboardapp.R;
+import com.example.gadsleaderboardapp.submit.SubmitActivity;
 import com.google.android.material.tabs.TabLayout;
 
 public class LeaderBoardActivity extends AppCompatActivity {
     private Toolbar toolbar;
-    private Button button;
+    private Button submit_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
 
-        toolbar =  findViewById(R.id.tool_bar);
+        toolbar =  findViewById(R.id.tool_bar_leaderboard);
+
         setSupportActionBar(toolbar);
 
-        button = findViewById(R.id.submit_button);
+        submit_button = findViewById(R.id.submit_button);
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LeaderBoardActivity.this, SubmitActivity.class);
+                startActivity(intent);
+            }
+        });
 
         LeaderBoardPagerAdapter householdPagerAdapter = new LeaderBoardPagerAdapter(getSupportFragmentManager());
         ViewPager viewPager =  findViewById(R.id.pager);
--- app/src/main/java/com/example/gadsleaderboardapp/submit/SubmitActivity.java
+++ app/src/main/java/com/example/gadsleaderboardapp/submit/SubmitActivity.java
@@ -1,8 +1,11 @@
 package com.example.gadsleaderboardapp.submit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 import android.os.Bundle;
+
 
 import com.example.gadsleaderboardapp.R;
 
@@ -12,5 +15,11 @@ public class SubmitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_submit_layout);
+
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_keyboard_backspace_24);
+
+        setSupportActionBar(toolbar);
     }
 }
